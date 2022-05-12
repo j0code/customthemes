10371670ddc90ec63662576059012384f383c8bc
j0code <jonaslp759@gmail.com>
2022-05-12 20:47:29 +0200

    initial crime

---
+++ background.js
@@ -0,0 +1,9 @@
+// Relevant apis: contentSettings, contextMenu, cookies, declarativeContent, declarativeNetRequest, devtools.network, i18n, identity, idle, loginState, management, notifications, omnibox, permissions, platformKeys, power, privacy, proxy, scripting, storage, system, webNavigation
+
+/*chrome.runtime.onInstalled.addListener(async () => {
+	let url = chrome.runtime.getURL("installed.html")
+	//let tab = await chrome.tabs.create({ url })
+	//console.log(`Created tab ${tab.id}`)
+})*/
+
+console.log("Oh yeah!")
